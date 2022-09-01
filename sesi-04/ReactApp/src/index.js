b081b7c0666c84861f84aea370a787356cf84964
Salman Alfarizi <salman.alfrz@upi.edu>
2022-09-01 22:31:18 +0700

    add sesi-04

---
+++ sesi-04/ReactApp/src/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { ReactDOM } from "react";
+
+const App = () => {
+    return (
+        <div>
+            <h2>Belajar React | First Application</h2>
+        </div>
+    )
+}
+
+ReactDOM.render(
+    <App/>,
+    document.getElementById('app')
+)
